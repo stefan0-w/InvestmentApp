f873c16b826f33520a64493cf4bc18608a41aded
Wojtek Stefanek <wojciast3@gmail.com>
2025-11-28 14:16:52 +0100

    db: add HistoricalPortfolioValue model and update Transaction date

--- backend/api/models.py
+++ backend/api/models.py
@@ -38,10 +38,19 @@ class Transaction(models.Model):
   quantity = models.DecimalField(max_digits=18, decimal_places=8)
   price = models.DecimalField(max_digits=18, decimal_places=8)
   transaction_type = models.CharField(max_length=4, choices=TRANSACTION_TYPE)
-  transaction_date = models.DateTimeField(auto_now_add=True)
+  transaction_date = models.DateTimeField()
 
   def __str__(self): 
     return f"{self.portfolio.user.username} - {self.transaction_type} {self.asset.symbol}"
 
 
+class HistoricalPortfolioValue(models.Model):
+    portfolio = models.ForeignKey(Portfolio, on_delete=models.CASCADE, related_name='history')
+    date = models.DateField()
+    value = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    profit_loss = models.DecimalField(max_digits=12, decimal_places=2, default=0)
 
+    class Meta:
+        # Zapewnia, że masz tylko jeden wpis na portfel na dany dzień
+        unique_together = ('portfolio', 'date')
+        ordering = ['date']
